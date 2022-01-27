1c858115ff0fe5920536f96b16ab4edc747e3df7
Kyle Smith <smithk86@gmail.com>
2022-01-27 15:51:05 -0500

    separate text formatter into function

--- sarcasticase.py
+++ sarcasticase.py
@@ -1,14 +1,10 @@
 import argparse
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("text", nargs="+")
-    args = parser.parse_args()
-
+def sarcasticase(text: str) -> str:
     _index = 0
     _text = ""
-    for _char in " ".join(args.text):
+    for _char in text:
         # do not incriment the index number for a space
         if _char == " ":
             _text += " "
@@ -16,7 +12,16 @@ def main():
             _mod = _index % 2
             _index += 1
             _text += _char.upper() if _mod == 1 else _char.lower()
-    print(_text)
+    return _text
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text", nargs="+")
+    args = parser.parse_args()
+
+    _text = " ".join(args.text)
+    print(sarcasticase(_text))
 
 
 if __name__ == "__main__":
